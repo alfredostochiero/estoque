e0b939c06385f95f0514491f005b8498cd0b8db0
unknown <alfredo_stochiero@hotmail.com>
2021-02-25 23:16:12 -0300

    configurando setting para imagens

--- django2/settings.py
+++ django2/settings.py
@@ -130,12 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 # arquivos de media :
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = str(BASE_DIR / 'media')
+
 
 
 
